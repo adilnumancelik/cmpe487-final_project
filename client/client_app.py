890fc3979db47792c216b9c5154e246b6e341bba
adilnumancelik2017400198 <44122592+adilnumancelik2017400198@users.noreply.github.com>
2021-01-03 20:49:19 +0300

    changes to client

--- client/client_app.py
+++ client/client_app.py
@@ -1,4 +1,3 @@
-import network
 import utils
 import time
 import requests
@@ -6,32 +5,51 @@ import socket
 import threading
 import json
 from game import Game
-'''
+from game_controller import GameController
+
 f = requests.request('GET', 'http://myip.dnsomatic.com')
 MY_IP = str(f.text)
+# MY_IP = "188.3.160.244"
 print(MY_IP)
-'''
-
-game = Game(10,10)
-MY_IP = "188.3.160.244"
-print(MY_IP)
-MY_PORT = 12345
+PORT = 12345
 SERVER_IP = "52.203.72.10"
 SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SERVER.settimeout(25.0)
-try:
-    SERVER.connect((SERVER_IP, MY_PORT))
-except:
-    print("Server does not respond.")
-    exit()
-PLAYER_NAME = "keko1"
-print(PLAYER_NAME)
 
-message_object= {"TYPE": "NAME","PAYLOAD": PLAYER_NAME}
-message=json.dumps(message_object)
+def initialize():
+    try:
+        SERVER.connect((SERVER_IP, MY_PORT))
+    except:
+        print("Server does not respond.")
+        exit()
+    PLAYER_NAME = "keko1"
+    print(PLAYER_NAME)
 
-network.send_message(SERVER, SERVER_IP, MY_PORT, message)
-y=threading.Thread(target=network.listen_to_server, args=(SERVER, ), daemon=True)
-y.start()
-print(message)
-y.join()
+    message_object= {"TYPE": "NAME","PAYLOAD": PLAYER_NAME}
+    message=json.dumps(message_object)
+
+    send_message(SERVER, SERVER_IP, MY_PORT, message)
+    y=threading.Thread(target=listen_to_server, args=(SERVER, ), daemon=True)
+    y.start()
+    print(message)
+    y.join()
+
+def send_message(message):
+    try:
+        server.sendall(utils.string_to_byte(message))
+    except:
+        print("Message could not be sent.")
+
+def listen_to_server():
+    control = GameController()
+    while True:
+        try:
+            from_server = SERVER.recv(4096)
+            # threading.Thread(target=control.process_message, args=(from_server, ), daemon=True).start()
+            control.process_message(from_server)
+        except: 
+            print("Server timed out.")
+            return False   
+
+
+initalize()
