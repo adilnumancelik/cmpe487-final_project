574f7ca3e42904f4cfb4d7d49ac02ed307c488eb
adilnumancelik2017400198 <44122592+adilnumancelik2017400198@users.noreply.github.com>
2021-01-03 14:51:02 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,22 @@
+import socket
+import os
+import threading
+import _thread
+
+MY_IP = a
+MY_PORT = b
+
+def server_thread_tcp():
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+
+    server.bind((MY_IP, MY_PORT)) 
+    server.listen()
+    while(1):
+      conn, addr = server.accept() 
+      with conn:
+        data = conn.recv(1024)
+        if not data:
+          continue
+
+        message = byte_to_string(data)
+        threading.Thread(target=respond_function, args=(message, ), daemon=True).start()
