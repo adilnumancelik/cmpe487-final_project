a04ca0fdf922f9786b6688c505272ba2e2da0ae9
ykaya932 <ykaya932@gmail.com>
2021-01-09 14:15:36 +0000

    Vay be

--- game/server/receiver.py
+++ game/server/receiver.py
@@ -4,16 +4,16 @@ import _thread
 import utils
 import json
 
-import GameController from game_controller
+from game_controller import GameController 
 
 PORT = 12345
 
 def accept_connections():
   controller = GameController()
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind(('0.0.0.0', MY_PORT)) 
+    server.bind(('0.0.0.0', PORT)) 
     server.listen()
-    print(f"Start to listen PORT: {MY_PORT}")
+    print(f"Start to listen PORT: {PORT}")
     while True:
       conn, addr = server.accept()
       with conn:
@@ -43,3 +43,5 @@ def threaded_client(conn, controller, id):
       controller.move(id, message["PAYLOAD"])
     elif message["TYPE"] == "ANSWER":
       controller.check_answer(id, message["PAYLOAD"])
+
+accept_connections()
